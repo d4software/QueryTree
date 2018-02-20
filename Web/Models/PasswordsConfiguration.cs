32c4b2eb803319531b6c7fc2cb5faf71180b26ef
Daniel Thompson <dan@d4software.co.uk>
2018-02-20 10:23:41 +0000

    Made the keyfile location configurable

---
+++ Web/Models/PasswordsConfiguration.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QueryTree.Models
+{
+    public class PasswordsConfiguration
+    {
+        public string Keyfile { get; set; }
+    }
+}
